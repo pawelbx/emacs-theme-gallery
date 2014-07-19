778eb97546eb1a48345f6f9e99177f30f4ef247b
Pawel Bokota <pawelb.lnx@gmail.com>
2014-07-19 19:29:00 -0400

    dynamicaly write js properties + buffer cleanup

---
+++ script/themeProperties.js
@@ -0,0 +1,6 @@
+var emacsThemesGallery = {};
+emacsThemesGallery.themeProperties = {};
+emacsThemesGallery.themeProperties.themes = [
+{ name: 'ample-light', dark: false},
+{ name: 'ample', dark: true}
+];
