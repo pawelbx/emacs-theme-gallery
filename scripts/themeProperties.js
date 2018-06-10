80831141adf5630a72a7dd744260a613378398c1
pawel <pawelb.lnx@gmail.com>
2018-06-09 20:22:07 -0400

    add spacemacs theme melpa property

--- scripts/themeProperties.js
+++ scripts/themeProperties.js
@@ -1,65 +1,305 @@
 var emacsThemesGallery = {};
 emacsThemesGallery.themeProperties = {};
 emacsThemesGallery.themeProperties.themes = [
-{ name: 'adwaita', dark: false, location: 'native', melpaName: '' },
-{ name: 'afternoon', dark: true, location: 'https://github.com/osener/emacs-afternoon-theme', melpaName: 'afternoon-theme' },
-{ name: 'alect-black', dark: true, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'alect-black-alt', dark: true, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'alect-dark', dark: true, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'alect-dark-alt', dark: true, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'alect-light', dark: false, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'alect-light-alt', dark: false, location: 'https://github.com/alezost/alect-themes', melpaName: 'alect-themes' },
-{ name: 'ample', dark: true, location: 'https://github.com/jordonbiondo/ample-theme', melpaName: 'ample-theme' },
-{ name: 'ample-light', dark: false, location: 'https://github.com/jordonbiondo/ample-theme', melpaName: 'ample-theme' },
-{ name: 'ample-zen', dark: true, location: 'https://github.com/mjwall/ample-zen', melpaName: 'ample-zen-theme' },
-{ name: 'anti-zenburn', dark: false, location: 'https://github.com/m00natic/anti-zenburn-theme', melpaName: 'anti-zenburn-theme' },
-{ name: 'badger', dark: true, location: 'https://github.com/ccann/badger-theme', melpaName: 'badger-theme' },
-{ name: 'bubbleberry', dark: true, location: 'https://github.com/jasonm23/emacs-bubbleberry-theme', melpaName: 'bubbleberry-theme' },
-{ name: 'busybee', dark: true, location: 'https://github.com/mswift42/busybee-theme', melpaName: 'busybee-theme' },
-{ name: 'cherry-blossom', dark: true, location: 'https://github.com/byels/emacs-cherry-blossom-theme', melpaName: 'cherry-blossom-theme' },
-{ name: 'clues', dark: true, location: 'https://github.com/emacsfodder/emacs-clues-theme', melpaName: 'clues-theme' },
-{ name: 'cyberpunk', dark: true, location: 'https://github.com/n3mo/cyberpunk-theme.el', melpaName: 'cyberpunk-theme' },
-{ name: 'dakrone', dark: true, location: 'https://github.com/dakrone/dakrone-theme', melpaName: 'dakrone-theme' },
-{ name: 'darkburn', dark: true, location: 'https://github.com/gorauskas/darkburn-theme', melpaName: 'darkburn-theme' },
-{ name: 'deeper-blue', dark: true, location: 'native', melpaName: '' },
-{ name: 'dichromacy', dark: false, location: 'native', melpaName: '' },
-{ name: 'distinguished', dark: true, location: 'https://github.com/Lokaltog/distinguished-theme', melpaName: 'distinguished-theme' },
-{ name: 'espresso', dark: false, location: 'https://github.com/dgutov/espresso-theme', melpaName: 'espresso-theme' },
-{ name: 'firebelly', dark: true, location: 'https://github.com/startling/firebelly', melpaName: 'firebelly-theme' },
-{ name: 'flatland', dark: true, location: 'https://github.com/gchp/flatland-emacs', melpaName: 'flatland-theme' },
-{ name: 'flatui', dark: false, location: 'https://github.com/Corsair/emacs-flatui-theme', melpaName: 'flatui-theme' },
-{ name: 'grandshell', dark: true, location: 'https://github.com/steckerhalter/grandshell-theme', melpaName: 'grandshell-theme' },
-{ name: 'gruber-darker', dark: true, location: 'https://github.com/rexim/gruber-darker-theme', melpaName: 'gruber-darker-theme' },
-{ name: 'gruvbox', dark: true, location: 'https://github.com/Greduan/emacs-theme-gruvbox', melpaName: 'gruvbox-theme' },
-{ name: 'hemisu-dark', dark: true, location: 'https://github.com/andrzejsliwa/hemisu-theme', melpaName: 'hemisu-theme' },
-{ name: 'hemisu-light', dark: false, location: 'https://github.com/andrzejsliwa/hemisu-theme', melpaName: 'hemisu-theme' },
-{ name: 'heroku', dark: true, location: 'https://github.com/jonathanchu/heroku-theme', melpaName: 'heroku-theme' },
-{ name: 'leuven', dark: false, location: 'https://github.com/fniessen/emacs-leuven-theme', melpaName: 'leuven-theme' },
-{ name: 'light-blue', dark: false, location: 'native', melpaName: '' },
-{ name: 'manoj-dark', dark: true, location: 'native', melpaName: '' },
-{ name: 'misterioso', dark: true, location: 'native', melpaName: '' },
-{ name: 'moe-dark', dark: true, location: 'https://github.com/kuanyui/moe-theme.el', melpaName: 'moe-theme' },
-{ name: 'moe-light', dark: false, location: 'https://github.com/kuanyui/moe-theme.el', melpaName: 'moe-theme' },
-{ name: 'monokai', dark: true, location: 'https://github.com/oneKelvinSmith/monokai-emacs', melpaName: 'monokai-theme' },
-{ name: 'nova', dark: true, location: 'https://github.com/muirmanders/emacs-nova-theme', melpaName: 'nova-theme' },
-{ name: 'sanityinc-tomorrow-blue', dark: true, location: 'https://github.com/purcell/color-theme-sanityinc-tomorrow', melpaName: 'color-theme-sanityinc-tomorrow' },
-{ name: 'sanityinc-tomorrow-bright', dark: true, location: 'https://github.com/purcell/color-theme-sanityinc-tomorrow', melpaName: 'color-theme-sanityinc-tomorrow' },
-{ name: 'sanityinc-tomorrow-day', dark: false, location: 'https://github.com/purcell/color-theme-sanityinc-tomorrow', melpaName: 'color-theme-sanityinc-tomorrow' },
-{ name: 'sanityinc-tomorrow-eighties', dark: true, location: 'https://github.com/purcell/color-theme-sanityinc-tomorrow', melpaName: 'color-theme-sanityinc-tomorrow' },
-{ name: 'sanityinc-tomorrow-night', dark: true, location: 'https://github.com/purcell/color-theme-sanityinc-tomorrow', melpaName: 'color-theme-sanityinc-tomorrow' },
-{ name: 'solarized-dark', dark: true, location: 'https://github.com/bbatsov/solarized-emacs', melpaName: 'solarized-theme' },
-{ name: 'solarized-light', dark: false, location: 'https://github.com/bbatsov/solarized-emacs', melpaName: 'solarized-theme' },
-{ name: 'soothe', dark: true, location: 'https://github.com/emacsfodder/emacs-soothe-theme', melpaName: 'soothe-theme' },
-{ name: 'spacemacs-dark', dark: true, location: 'https://github.com/nashamri/spacemacs-theme', melpaName: '' },
-{ name: 'spacemacs-light', dark: true, location: 'https://github.com/nashamri/spacemacs-theme', melpaName: '' },
-{ name: 'tango', dark: false, location: 'native', melpaName: '' },
-{ name: 'tango-dark', dark: true, location: 'native', melpaName: '' },
-{ name: 'tangotango', dark: true, location: 'https://github.com/juba/color-theme-tangotango', melpaName: 'tangotango-theme' },
-{ name: 'tango-plus', dark: false, location: 'https://github.com/tmalsburg/tango-plus-theme', melpaName: 'tango-plus-theme' },
-{ name: 'tsdh-dark', dark: true, location: 'native', melpaName: '' },
-{ name: 'tsdh-light', dark: false, location: 'native', melpaName: '' },
-{ name: 'wheatgrass', dark: true, location: 'native', melpaName: '' },
-{ name: 'whiteboard', dark: false, location: 'native', melpaName: '' },
-{ name: 'wombat', dark: true, location: 'native', melpaName: '' },
-{ name: 'zenburn', dark: true, location: 'https://github.com/bbatsov/zenburn-emacs', melpaName: 'zenburn-theme' }
+  { name: "adwaita", dark: false, location: "native", melpaName: "" },
+  {
+    name: "afternoon",
+    dark: true,
+    location: "https://github.com/osener/emacs-afternoon-theme",
+    melpaName: "afternoon-theme"
+  },
+  {
+    name: "alect-black",
+    dark: true,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "alect-black-alt",
+    dark: true,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "alect-dark",
+    dark: true,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "alect-dark-alt",
+    dark: true,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "alect-light",
+    dark: false,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "alect-light-alt",
+    dark: false,
+    location: "https://github.com/alezost/alect-themes",
+    melpaName: "alect-themes"
+  },
+  {
+    name: "ample",
+    dark: true,
+    location: "https://github.com/jordonbiondo/ample-theme",
+    melpaName: "ample-theme"
+  },
+  {
+    name: "ample-light",
+    dark: false,
+    location: "https://github.com/jordonbiondo/ample-theme",
+    melpaName: "ample-theme"
+  },
+  {
+    name: "ample-zen",
+    dark: true,
+    location: "https://github.com/mjwall/ample-zen",
+    melpaName: "ample-zen-theme"
+  },
+  {
+    name: "anti-zenburn",
+    dark: false,
+    location: "https://github.com/m00natic/anti-zenburn-theme",
+    melpaName: "anti-zenburn-theme"
+  },
+  {
+    name: "badger",
+    dark: true,
+    location: "https://github.com/ccann/badger-theme",
+    melpaName: "badger-theme"
+  },
+  {
+    name: "bubbleberry",
+    dark: true,
+    location: "https://github.com/jasonm23/emacs-bubbleberry-theme",
+    melpaName: "bubbleberry-theme"
+  },
+  {
+    name: "busybee",
+    dark: true,
+    location: "https://github.com/mswift42/busybee-theme",
+    melpaName: "busybee-theme"
+  },
+  {
+    name: "cherry-blossom",
+    dark: true,
+    location: "https://github.com/byels/emacs-cherry-blossom-theme",
+    melpaName: "cherry-blossom-theme"
+  },
+  {
+    name: "clues",
+    dark: true,
+    location: "https://github.com/emacsfodder/emacs-clues-theme",
+    melpaName: "clues-theme"
+  },
+  {
+    name: "cyberpunk",
+    dark: true,
+    location: "https://github.com/n3mo/cyberpunk-theme.el",
+    melpaName: "cyberpunk-theme"
+  },
+  {
+    name: "dakrone",
+    dark: true,
+    location: "https://github.com/dakrone/dakrone-theme",
+    melpaName: "dakrone-theme"
+  },
+  {
+    name: "darkburn",
+    dark: true,
+    location: "https://github.com/gorauskas/darkburn-theme",
+    melpaName: "darkburn-theme"
+  },
+  { name: "deeper-blue", dark: true, location: "native", melpaName: "" },
+  { name: "dichromacy", dark: false, location: "native", melpaName: "" },
+  {
+    name: "distinguished",
+    dark: true,
+    location: "https://github.com/Lokaltog/distinguished-theme",
+    melpaName: "distinguished-theme"
+  },
+  {
+    name: "espresso",
+    dark: false,
+    location: "https://github.com/dgutov/espresso-theme",
+    melpaName: "espresso-theme"
+  },
+  {
+    name: "firebelly",
+    dark: true,
+    location: "https://github.com/startling/firebelly",
+    melpaName: "firebelly-theme"
+  },
+  {
+    name: "flatland",
+    dark: true,
+    location: "https://github.com/gchp/flatland-emacs",
+    melpaName: "flatland-theme"
+  },
+  {
+    name: "flatui",
+    dark: false,
+    location: "https://github.com/Corsair/emacs-flatui-theme",
+    melpaName: "flatui-theme"
+  },
+  {
+    name: "grandshell",
+    dark: true,
+    location: "https://github.com/steckerhalter/grandshell-theme",
+    melpaName: "grandshell-theme"
+  },
+  {
+    name: "gruber-darker",
+    dark: true,
+    location: "https://github.com/rexim/gruber-darker-theme",
+    melpaName: "gruber-darker-theme"
+  },
+  {
+    name: "gruvbox",
+    dark: true,
+    location: "https://github.com/Greduan/emacs-theme-gruvbox",
+    melpaName: "gruvbox-theme"
+  },
+  {
+    name: "hemisu-dark",
+    dark: true,
+    location: "https://github.com/andrzejsliwa/hemisu-theme",
+    melpaName: "hemisu-theme"
+  },
+  {
+    name: "hemisu-light",
+    dark: false,
+    location: "https://github.com/andrzejsliwa/hemisu-theme",
+    melpaName: "hemisu-theme"
+  },
+  {
+    name: "heroku",
+    dark: true,
+    location: "https://github.com/jonathanchu/heroku-theme",
+    melpaName: "heroku-theme"
+  },
+  {
+    name: "leuven",
+    dark: false,
+    location: "https://github.com/fniessen/emacs-leuven-theme",
+    melpaName: "leuven-theme"
+  },
+  { name: "light-blue", dark: false, location: "native", melpaName: "" },
+  { name: "manoj-dark", dark: true, location: "native", melpaName: "" },
+  { name: "misterioso", dark: true, location: "native", melpaName: "" },
+  {
+    name: "moe-dark",
+    dark: true,
+    location: "https://github.com/kuanyui/moe-theme.el",
+    melpaName: "moe-theme"
+  },
+  {
+    name: "moe-light",
+    dark: false,
+    location: "https://github.com/kuanyui/moe-theme.el",
+    melpaName: "moe-theme"
+  },
+  {
+    name: "monokai",
+    dark: true,
+    location: "https://github.com/oneKelvinSmith/monokai-emacs",
+    melpaName: "monokai-theme"
+  },
+  {
+    name: "nova",
+    dark: true,
+    location: "https://github.com/muirmanders/emacs-nova-theme",
+    melpaName: "nova-theme"
+  },
+  {
+    name: "sanityinc-tomorrow-blue",
+    dark: true,
+    location: "https://github.com/purcell/color-theme-sanityinc-tomorrow",
+    melpaName: "color-theme-sanityinc-tomorrow"
+  },
+  {
+    name: "sanityinc-tomorrow-bright",
+    dark: true,
+    location: "https://github.com/purcell/color-theme-sanityinc-tomorrow",
+    melpaName: "color-theme-sanityinc-tomorrow"
+  },
+  {
+    name: "sanityinc-tomorrow-day",
+    dark: false,
+    location: "https://github.com/purcell/color-theme-sanityinc-tomorrow",
+    melpaName: "color-theme-sanityinc-tomorrow"
+  },
+  {
+    name: "sanityinc-tomorrow-eighties",
+    dark: true,
+    location: "https://github.com/purcell/color-theme-sanityinc-tomorrow",
+    melpaName: "color-theme-sanityinc-tomorrow"
+  },
+  {
+    name: "sanityinc-tomorrow-night",
+    dark: true,
+    location: "https://github.com/purcell/color-theme-sanityinc-tomorrow",
+    melpaName: "color-theme-sanityinc-tomorrow"
+  },
+  {
+    name: "solarized-dark",
+    dark: true,
+    location: "https://github.com/bbatsov/solarized-emacs",
+    melpaName: "solarized-theme"
+  },
+  {
+    name: "solarized-light",
+    dark: false,
+    location: "https://github.com/bbatsov/solarized-emacs",
+    melpaName: "solarized-theme"
+  },
+  {
+    name: "soothe",
+    dark: true,
+    location: "https://github.com/emacsfodder/emacs-soothe-theme",
+    melpaName: "soothe-theme"
+  },
+  {
+    name: "spacemacs-dark",
+    dark: true,
+    location: "https://github.com/nashamri/spacemacs-theme",
+    melpaName: "spacemacs-theme"
+  },
+  {
+    name: "spacemacs-light",
+    dark: true,
+    location: "https://github.com/nashamri/spacemacs-theme",
+    melpaName: "spacemacs-theme"
+  },
+  { name: "tango", dark: false, location: "native", melpaName: "" },
+  { name: "tango-dark", dark: true, location: "native", melpaName: "" },
+  {
+    name: "tangotango",
+    dark: true,
+    location: "https://github.com/juba/color-theme-tangotango",
+    melpaName: "tangotango-theme"
+  },
+  {
+    name: "tango-plus",
+    dark: false,
+    location: "https://github.com/tmalsburg/tango-plus-theme",
+    melpaName: "tango-plus-theme"
+  },
+  { name: "tsdh-dark", dark: true, location: "native", melpaName: "" },
+  { name: "tsdh-light", dark: false, location: "native", melpaName: "" },
+  { name: "wheatgrass", dark: true, location: "native", melpaName: "" },
+  { name: "whiteboard", dark: false, location: "native", melpaName: "" },
+  { name: "wombat", dark: true, location: "native", melpaName: "" },
+  {
+    name: "zenburn",
+    dark: true,
+    location: "https://github.com/bbatsov/zenburn-emacs",
+    melpaName: "zenburn-theme"
+  }
 ];
